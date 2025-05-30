88bc5d2c768ace03f829711256d7491b80dc254d
Mahmoud Allam <92165435+mahmoud530@users.noreply.github.com>
2025-05-30 22:25:30 +0300

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+
+window.onscroll = () => {
+  const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById("scroll-line").style.width = scrolled + "%";
+};
