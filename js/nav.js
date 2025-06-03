83bed990c4f6aa7c720cf2fabb860081dd6176c6
Mahmoud Allam <92165435+mahmoud530@users.noreply.github.com>
2025-06-03 20:46:26 +0300

    Delete js/nav.js

--- js/nav.js
+++
@@ -1,7 +0,0 @@
-
-window.onscroll = () => {
-  const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
-  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const scrolled = (winScroll / height) * 100;
-  document.getElementById("scroll-line").style.width = scrolled + "%";
-};
