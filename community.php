84792b60acc4e7006ec0d6d258659cc30905a878
Salmamohamed33 <140505053+Salmamohamed33@users.noreply.github.com>
2025-06-11 03:36:27 +0300

    Add files via upload

--- community.php
+++ community.php
@@ -1,5 +1,5 @@
 <?php
-include 'connection.php';
+include "nav.php";
 
 if(!isset($_SESSION['user_id'])){
      header("location: signup&login.php");
@@ -170,6 +170,8 @@ if (isset($_POST['action']) && $_POST['action'] == 'delete_post') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/community.css">
     <title>Community</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <style>
     body {
   margin: 0;
@@ -386,63 +388,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'delete_post') {
 <body>
 
 
-<div class="top-header">
-  <div class="contact d-flex align-items-center">
-    <i class="fas fa-phone me-2"></i>012-201-77444
-  </div>
-  <div class="top-actions">
-    <a href="contact_us.php" class="angled-section get-touch">
-      <i class="fas fa-comments me-2"></i> Get in Touch
-    </a>
-    <a href="#" class="angled-section book-tour">
-      <i class="fas fa-calendar-alt me-2"></i> Book a Tour
-    </a>
-  </div>
-  <div class="social-icons">
-    <a href="#"><i class="fab fa-facebook-f"></i></a>
-    <a href="#"><i class="fab fa-instagram"></i></a>
-    <a href="#"><i class="fab fa-linkedin-in"></i></a>
-  </div>
-</div>
 
-<!-- Logo & Navigation bottom-->
-<div class="logo-bar">
-  <div class="logo">
-    <img src="./img/SCCI_Logo.png" alt="Logo">
-    <p>SCCI Workspaces</p>
-  </div>
-  <nav class="navbar navbar-expand-lg navbar-light">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainN
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="mainNav">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 bottom-nav">
-        <li class="nav-item"><a class="nav-link" href="indexx.php">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="my_bookings.php">My Bookings</a></li>
-        <li class="nav-item"><a class="nav-link" href="workspaces_list.php">Workspaces</a></li>
-        <li class="nav-item"><a class="nav-link" href="community.php">Community</a></li>
-
-       <li class="nav-item dropdown">
-         <span id="moreDropdown" class="nav-link more-link" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="cursor: pointer;">
-           More
-         </span>
-         <ul class="dropdown-menu more-dropdown-menu" aria-labelledby="moreDropdown">
-           <li><a class="dropdown-item" href="chat.php">Chat</a></li>
-           <li><a class="dropdown-item" href="calendar.php">Calendar</a></li>
-         </ul>
-       </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="profile.php">
-            <i class="fas fa-user profile-icon"></i>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  <div id="scroll-line"></div>
-
-</div>
 
     
  
