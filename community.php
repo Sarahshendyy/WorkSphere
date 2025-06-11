5c5c4270daabf104069f8eec901808af6e85dfd1
Sarah Shendy <sarahshendy32@gmail.com>
2025-06-11 22:49:06 +0300

    Add files via upload

--- community.php
+++ community.php
@@ -1,6 +1,6 @@
 <?php
-include "nav.php";
-
+// include "nav.php";
+include "connection.php";
 if(!isset($_SESSION['user_id'])){
      header("location: signup&login.php");
 }
@@ -386,13 +386,101 @@ if (isset($_POST['action']) && $_POST['action'] == 'delete_post') {
 
 
 <body>
+ <div class="top-header">
+  <div class="contact d-flex align-items-center">
+    <i class="fas fa-phone me-2"></i>012-201-77444
+  </div>
+  <div class="top-actions">
+    <a href="about_us.php" class="angled-section get-touch">
+      <i class="fas fa-comments me-2"></i> About Us
+    </a>
+
+    <?php if (!isset($_SESSION['role_id']) || $_SESSION['role_id'] != 3): ?>
+      <a href="workspaces_list.php" class="angled-section book-tour">
+        <i class="fas fa-calendar-alt me-2"></i> Book a Workspace
+      </a>
+    <?php endif; ?>
 
 
+  </div>
+  <div class="social-icons">
+    <a href="#"><i class="fab fa-facebook-f"></i></a>
+    <a href="#"><i class="fab fa-instagram"></i></a>
+    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+  </div>
+</div>
 
+<!-- Logo & Navigation bottom-->
+<div class="logo-bar">
+  <div class="logo">
+    <a href="indexx.php">
+      <img src="./img/logo.png" alt="Logo">
+    </a>
+    <!-- <p>WorkSohere</p> -->
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mainNav">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 bottom-nav">
+       <?php
+    if (isset($_SESSION['role_id'])) {
+        if ($_SESSION['role_id'] == 3) {
+            // Workspace Owner
+            echo '<li class="nav-item">
+                    <a class="nav-link" href="./workspace/workspaces_dashboard.php">
+                        Return to My Interface
+                    </a>
+                  </li>';
+        } elseif ($_SESSION['role_id'] == 4) {
+            // Admin
+            echo '<li class="nav-item">
+                    <a class="nav-link" href="./admin/admin_dashboard.php">
+                        Return to My Interface
+                    </a>
+                  </li>';
+        }
+    }
+    ?>
+        <li class="nav-item"><a class="nav-link" href="indexx.php">Home</a></li>
 
-    
- 
+        <?php if (isset($_SESSION['role_id']) && $_SESSION['role_id'] == 3): ?>
+          <li class="nav-item"><a class="nav-link" href="./workspace/workspaces_dashboard.php">My Dashboard</a></li>
+        <?php else: ?>
+          <li class="nav-item"><a class="nav-link" href="my_bookings.php">My Bookings</a></li>
+        <?php endif; ?>
 
+        <li class="nav-item"><a class="nav-link" href="workspaces_list.php">Workspaces</a></li>
+        <li class="nav-item"><a class="nav-link" href="community.php">Community</a></li>
+
+        <li class="nav-item dropdown">
+          <span id="moreDropdown" class="nav-link more-link" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="cursor: pointer;">
+            More
+          </span>
+          <ul class="dropdown-menu more-dropdown-menu" aria-labelledby="moreDropdown">
+            <li><a class="dropdown-item" href="chat.php">Chat</a></li>
+            <li><a class="dropdown-item" href="calendar.php">Calendar</a></li>
+           <li><a class="dropdown-item" href="fav_workspaces.php">Favourites</a></li>
+          </ul>
+        </li>
+        <?php if(isset($_SESSION['user_id'])) { ?>
+        <li class="nav-item">
+          <a class="nav-link" href="profile.php">
+            <i class="fas fa-user profile-icon"></i>
+          </a>
+        </li>
+        <?php } else { ?>
+        <li class="nav-item">
+          <a class="nav-link" href="signup&login.php">Login</a>
+        </li>
+        <?php } ?>
+        
+      </ul>
+    </div>
+  </nav>
+  <div id="scroll-line"></div>
+</div>
     <div class="container">
         <!-- header -->
         <header class="header">
