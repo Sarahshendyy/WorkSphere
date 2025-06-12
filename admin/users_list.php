3faa7ea259ebe622d8b6e34f857612f3627023e4
Sarah Shendy <sarahshendy32@gmail.com>
2025-06-12 05:51:46 +0300

    Update users_list.php

--- admin/users_list.php
+++ admin/users_list.php
@@ -37,11 +37,11 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id']))
             </div>
             <div style='background-color: #4B6382; padding: 10px; text-align: center; color: #fff;'>
                 <p>For urgent matters, please contact:</p>
-                <p>Email: <a href='mailto:admin-support@worksphere04@gmail.com' style='color: #A68868;'>admin-support@worksphere04@gmail.com</a></p>
+                <p>Email: <a href='mailto:admin-support@worksphere50@gmail.com' style='color: #A68868;'>admin-support@worksphere04@gmail.com</a></p>
             </div>
         </body>";
         
-        $mail->setFrom('worksphere04@gmail.com', 'WorkSphere');
+        $mail->setFrom('worksphere50@gmail.com', 'WorkSphere');
         $mail->addAddress($email);
         $mail->isHTML(true);
         $mail->Subject = 'Your WorkSphere Account Has Been Deleted';
@@ -97,11 +97,11 @@ if (isset($_GET['action']) && $_GET['action'] == 'hold' && isset($_GET['id'])) {
             </div>
             <div style='background-color: #4B6382; padding: 10px; text-align: center; color: #fff;'>
                 <p>For support or questions, please contact:</p>
-                <p>Email: <a href='mailto:admin-support@worksphere04@gmail.com' style='color: #A68868;'>admin-support@worksphere04@gmail.com</a></p>
+                <p>Email: <a href='mailto:admin-support@worksphere50@gmail.com' style='color: #A68868;'>admin-support@worksphere50@gmail.com</a></p>
             </div>
         </body>";
         
-        $mail->setFrom('worksphere04@gmail.com', 'WorkSphere');
+        $mail->setFrom('worksphere50@gmail.com', 'WorkSphere');
         $mail->addAddress($email);
         $mail->isHTML(true);
         $mail->Subject = 'Your WorkSphere Account Has Been Placed On Hold';
