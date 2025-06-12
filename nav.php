5840124ea6ea1162ce46a001c745c4781d6ef6f9
Sarah Shendy <sarahshendy32@gmail.com>
2025-06-12 16:29:15 +0300

    Update nav.php

--- nav.php
+++ nav.php
@@ -38,11 +38,7 @@
 
 
   </div>
-  // <div class="social-icons">
-  //   <a href="#"><i class="fab fa-facebook-f"></i></a>
-  //   <a href="#"><i class="fab fa-instagram"></i></a>
-  //   <a href="#"><i class="fab fa-linkedin-in"></i></a>
-  // </div>
+  
 </div>
 
 <!-- Logo & Navigation bottom-->
