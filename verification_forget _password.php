721edae4807874a617d600f989a3daa11967d04d
Sarah Shendy <sarahshendy32@gmail.com>
2025-06-12 06:08:23 +0300

    Update verification_forget _password.php

--- verification_forget _password.php
+++ verification_forget _password.php
@@ -56,7 +56,7 @@ if (isset($_POST['resend'])){
     </div>
     <div style='background-color: #071739; padding: 10px; text-align: center; color: #CDD5DB;'>
         <p>For support and updates, please visit our website or contact us via email.</p>
-        <p>Email: <a href='mailto:worksphere04@gmail.com' style='color:rgb(218, 166, 103); text-decoration: none;'>worksphere04@gmail.com</a></p>
+        <p>Email: <a href='mailto:worksphere50@gmail.com' style='color:rgb(218, 166, 103); text-decoration: none;'>worksphere50@gmail.com</a></p>
     </div>
 </body>
 ";
@@ -66,7 +66,7 @@ if (isset($_POST['resend'])){
      $old_time=time();  // AS IT IS
      $_SESSION['time']=$old_time;
 
-     $mail->setFrom('worksphere04@gmail.com', 'WorkSphere');
+     $mail->setFrom('worksphere50@gmail.com', 'WorkSphere');
      $mail->addAddress($email);
      $mail->isHTML(true);
      $mail->Subject = 'Password Reset OTP';
